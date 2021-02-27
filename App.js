4eb2ec60424c3f245e4eb85c36bf46e27ae0245c
hannahn-boop <75150800+hannahn-boop@users.noreply.github.com>
2021-02-27 18:06:21 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen';
+//import { PermissionsAndroid } from 'react-native';
+
 
 const MainStack = createStackNavigator({
   Home: { 
@@ -15,3 +17,23 @@ const MainStack = createStackNavigator({
 console.disableYellowBox = true;
 const App = new createAppContainer(MainStack);
 export default App;
+
+
+
+//location permission 
+//export async function requestLocationPermission() 
+//{
+  //try {
+    //const granted = await PermissionsAndroid.request(
+      //PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION)
+    //if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      //console.log("You can use the location")
+      //alert("You can use the location");
+    //} else {
+      //console.log("location permission denied")
+      //alert("Location permission denied");
+    //}
+  //} catch (err) {
+    //console.warn(err)
+  //}
+//}
