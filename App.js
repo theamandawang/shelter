30bd7afccafe8f0751575db3108719da1bc0aa5e
Amanda <mohiganscs@gmail.com>
2021-02-27 15:02:04 -0500

    testing

--- App.js
+++ App.js
@@ -1,6 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import AboutScreen from './src/screens/AboutScreen';
+import SearchRecipesScreen from './src/screens/SearchRecipes';
+import HomeScreen from './src/screens/HomeScreen';
+const MainStack = createStackNavigator({
+  Home: { 
+    screen: HomeScreen // The main screen of the app. Has all the buttons
+  },
+  Recipes: {
+    screen: SearchRecipesScreen
+  },
+  About: {
+    screen: AboutScreen
+  },
+  initialRouteName: 'Home',
+  headerMode: 'none',
+});
+
+
+// Disables Yellow text on the Virtual Device
+console.disableYellowBox = true;
+const App = new createAppContainer(MainStack);
+export default App;
+
+
+
+
 
 export default function App() {
   return (
