63b2c35607eb989a4fc551d111d1f6b9ab78cbf8
Amanda <mohiganscs@gmail.com>
2021-02-28 11:23:00 -0500

    hard coded markers

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,5 +1,5 @@
 import React, {Component, useState, useEffect} from 'react'
-import { View, Text, Platform, Alert} from 'react-native'
+import { View, Text, Platform, Alert, Linking} from 'react-native'
 import * as Location from 'expo-location';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'
 import { Appbar } from 'react-native-paper'
@@ -69,7 +69,43 @@ const alert = () =>
             showsUserLocation={true}
             provider = {PROVIDER_GOOGLE}
           >
+
+              <MapView.Marker 
+                key = {0}
+                title= {'The Bartlett House'}
+                description= {'1110 University Ave, Morgantown, WV 26505\nPhone: (304) 292-0101'}
+                coordinate={{
+                  latitude: 39.629150,
+                  longitude: -79.958610
+                }}
+                onPress={() =>Linking.openURL('https://www.bartletthousingsolutions.org/')}
+                pinColor={'#ff0000'}
+              />
+              <MapView.Marker 
+                key = {1}
+                title= {'The United Way'}
+                description= {'278 Spruce St, Morgantown, WV 26505\nPhone: (304) 296-7525'}
+                coordinate={{
+                  latitude: 39.629080,
+                  longitude: -79.955010
+                }}
+                onPress={() =>Linking.openURL('https://www.unitedwaympc.org/')}
+                pinColor={'#ff0000'}
+              />
+              <MapView.Marker 
+                key = {2}
+                title= {'Compass Women\'s Center'}
+                description= {'283 Don Knotts Blvd, Morgantown, WV 26501\nPhone: (304) 212-2131'}
+                coordinate={{
+                  latitude: 39.615050,
+                  longitude: -79.970210
+                }}
+                onPress={() =>Linking.openURL('https://compass4women.org/resources/')}
+                pinColor={'#ff0000'}
+              />
           </MapView>
+
+          
           <Appbar.Header
             dark = {true}
             style={{backgroundColor:'#bdbdbd'}}
